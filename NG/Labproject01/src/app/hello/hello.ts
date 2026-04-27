5f115c646f3d2f498a706da8c52783eed8f040b0
Hamir Dhanquer <hamirdhanquer@gmail.com>
2026-04-27 14:25:09 -0300

    Adicionando component

--- NG/Labproject01/src/app/hello/hello.ts
+++ NG/Labproject01/src/app/hello/hello.ts
@@ -6,4 +6,14 @@ import { Component } from '@angular/core';
   templateUrl: './hello.html',
   styleUrl: './hello.css',
 })
-export class Hello {}
+export class Hello {
+
+  protected title = 'Hello World - First Angular App';
+  protected isDisabled = false;
+
+  onClick() {
+    this.isDisabled = true;
+    console.log('Button clicked!');
+  }
+
+}
