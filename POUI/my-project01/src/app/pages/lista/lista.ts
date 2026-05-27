507399c830c2082c8dc42aa57739f4e016c560a1
Hamir Dhanquer <hamir@fundag.br>
2026-05-26 21:43:26 -0300

    Testando componentes

--- POUI/my-project01/src/app/pages/lista/lista.ts
+++ POUI/my-project01/src/app/pages/lista/lista.ts
@@ -14,6 +14,7 @@ import {
 export class Lista {
 
   minhaLista: string = "Minha Lista, PO UI, Angular, TypeScript";
+  listaFrutas 
   //Texto = output<string>();
   //Texto2 = input<string>("TESTE");
 
